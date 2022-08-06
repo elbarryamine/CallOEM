c06e05b41ee22fb4592f8e5fbcc878307666f356
elbarryamine <aminegtipsn@gmail.com>
2022-08-06 03:19:12 +0100

    refactor useBackHandler to use the useSpammming hook

--- app/src/shared/hooks/useBackHandler.ts
+++ app/src/shared/hooks/useBackHandler.ts
@@ -1,24 +1,17 @@
 import {useNavigation} from '@react-navigation/native';
 import {useEffect, useState} from 'react';
 import {BackHandler} from 'react-native';
+import useIsSpamming from './useIsSpamming';
 
 export default function useBackHandler() {
   const navigation = useNavigation();
   const [isAllowedToGoBack, setIsAllowedToGoBack] = useState<boolean>(true);
-  const [isSpamming, setIsSpamming] = useState<boolean>(false);
-  const [tries, setTries] = useState<number>(0);
+  const {setTrySpam, isSpamming} = useIsSpamming();
   function goBack() {
     navigation.goBack();
   }
   const callBack = () => {
-    setTries(prevTries => {
-      if (prevTries > 3) {
-        setIsSpamming(true);
-        return prevTries;
-      }
-      return prevTries + 1;
-    });
-
+    setTrySpam();
     return !isAllowedToGoBack;
   };
   useEffect(() => {
@@ -28,15 +21,7 @@ export default function useBackHandler() {
       BackHandler.removeEventListener('hardwareBackPress', callBack);
     };
   }, [isAllowedToGoBack]);
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsSpamming(false);
-      setTries(0);
-    }, 3000);
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [tries]);
+
   return {
     allowBack: () => setIsAllowedToGoBack(true),
     preventBack: () => setIsAllowedToGoBack(false),
